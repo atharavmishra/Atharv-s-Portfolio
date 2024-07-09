d8d052491aacaa2710bd96313068a61f25c5c6c5
atharavmishra <atharvmishra77@gmail.com>
2024-07-09 23:55:26 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">developed by Atharv</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -40,12 +40,13 @@ export default function Home() {
                 />
               </li>
               <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
+               <a
+      className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+      href="resume.pdf" // Path to your PDF file in the public directory
+      download="Atharvs_Resume.pdf" // Filename for the downloaded file
+    >
+      Resume
+    </a>
 
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
