8c8b92a99cfc3e04457d5357e440f123421b61a3
atharavmishra <atharvmishra77@gmail.com>
2024-07-09 13:38:00 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">Developed by Atharv</h1>
+            <h1 className="font-burtons text-xl">developedbyed</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -43,25 +43,15 @@ export default function Home() {
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="#"
-                  onClick={() => setDarkMode(!darkMode)}
                 >
                   Resume
-                  
                 </a>
-
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Github
-                </a>
-
               </li>
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Atharv
+              Dimitri Marco
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Developer and designer.
