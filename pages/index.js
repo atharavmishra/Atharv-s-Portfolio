541496a5da5d1f27acb16c1e2e2d2a66e92c67db
atharavmishra <atharvmishra77@gmail.com>
2024-07-09 15:40:56 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Atharv Mishra
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Developer and designer.
