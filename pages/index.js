a5b67a6e79700a645ba54b03a106361075d1eba2
atharavmishra <atharvmishra77@gmail.com>
2024-07-09 16:24:11 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -46,6 +46,13 @@ export default function Home() {
                 >
                   Resume
                 </a>
+
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="#"
+                >
+                  Github
+                </a>
               </li>
             </ul>
           </nav>
