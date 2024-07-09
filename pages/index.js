8fd253696df29e0667cce313ee8982e5e334d7bf
atharavmishra <atharvmishra77@gmail.com>
2024-07-09 16:38:48 +0530

    m

--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ export default function Home() {
               Atharv Mishra
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+              Android App developer.
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
               Freelancer providing services for programming and design content
