2bd957dce3b9c5df7803f8e8b0fef7101955e4c2
atharavmishra <atharvmishra77@gmail.com>
2024-07-10 17:11:17 +0530

    initial

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
               <li>
                <a
       className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-      href="resume.pdf" // Path to your PDF file in the public directory
+      href="atharva_mishra.pdf" // Path to your PDF file in the public directory
       download="Atharvs_Resume.pdf" // Filename for the downloaded file
     >
       Resume
