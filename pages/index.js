e504f6ad80a597a2f48a21e660f2d4293b9ba5e9
Atharv Mishra <atharvmishra77@gmail.com>
2025-06-29 20:07:48 +0530

    added self-checkout

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,15 @@ import wallpaper_app3 from "../public/wallpaper3.png";
 import nnpic1 from "../public/nnpic5.webp";
 import nnpic2 from "../public/nnpic1.webp";
 import nnpic3 from "../public/nnpic4.webp";
+import selfCheckout from "../public/self_checkout.png";
+
+import qq_1 from "../public/qq_1.png";
+import qq_2 from "../public/qq_2.png";
+import qq_3 from "../public/qq_3.png";
 import ustore1 from "../public/ustore_1.png";
 import ustore2 from "../public/ustore_2.png";
 import ustore3 from "../public/ustore_3.png";
+
 import Image from "next/image";
 import androidStudioIcon from "../public/android_studio_icon.png";
 import firebaseIcon from "../public/firebase.png";
@@ -56,6 +62,9 @@ export default function Home() {
 
   const handleRedirectNoNames = () => {
     window.open('https://play.google.com/store/apps/details?id=com.nonamessocialmedia&pcampaignid=web_share', '_blank');
+  };
+  const handleRedirectQQ = () => {
+    window.open('https://quickq.codesh.in/store?handles=2Rd9w69dG6aLmeEsBz6b', '_blank');
   };
 
   const handleRedirectUnnatiWeb = () => {
@@ -123,7 +132,7 @@ export default function Home() {
       </div>
     );
   };
-return (
+  return (
     <div className={`${darkMode ? "dark" : ""} w-full h-full`}>
       <Head>
         <title>Create Next App</title>
@@ -204,7 +213,7 @@ return (
                 </div>
                 <div className="flex items-center space-x-4">
                   <span className="text-2xl">⏰</span>
-                  <p>Developing <a className="text-purple-300">a calorie tracker app powered by AI</a></p>
+                  <p>working on<a className="text-purple-300">Restaurant automation application</a></p>
                 </div>
                 <div className="flex items-center space-x-4">
                   <div style={{ width: '28px', height: '28px', position: 'relative' }}>
@@ -301,7 +310,7 @@ return (
 
             </div>
           </div>
-          <div className="lg:flex gap-5">
+          {/* <div className="lg:flex gap-5">
             <div className="text-start md:p-10 rounded-xl mt-10 flex-1 flex flex-wrap" style={{ height: 'auto' }}>
               <div className="w-full lg:w-1/3 p-2">
                 <div className="shadow-2xl rounded-lg overflow-hidden bg-white dark:bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-xl" style={{ width: '100%', position: 'relative' }}>
@@ -337,7 +346,7 @@ return (
               </div>
 
             </div>
-          </div>
+          </div> */}
           <div className="lg:flex gap-5">
             <div className="text-start md:p-10 rounded-xl mt-10 flex-1 flex flex-wrap" style={{ height: 'auto' }}>
               <div className="w-full lg:w-1/3 p-2">
@@ -401,7 +410,7 @@ return (
                   No Names Social Media App
                 </h3>
                 <p className="py-2 text-white">
-                 Led the development of social media app which user can use to socialise anonymously.
+                  Led the development of social media app which user can use to socialise anonymously.
                 </p>
                 <div className="flex justify-center mt-6">
                   <a
@@ -417,8 +426,87 @@ return (
 
             </div>
           </div>
+
+          <div className="lg:flex gap-5">
+            <div className="text-start md:p-10 rounded-xl mt-10 flex-1 flex flex-wrap" style={{ height: 'auto' }}>
+              <div className="w-full lg:w-1/3 p-2">
+                <div className="shadow-2xl rounded-lg overflow-hidden bg-white dark:bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-xl" style={{ width: '100%', position: 'relative' }}>
+                  <Image src={selfCheckout} layout="responsive" objectFit="cover" />
+                </div>
+              </div>
+
+              <div className="w-full lg:w-1/3 p-2">
+                <div className="shadow-2xl rounded-lg overflow-hidden bg-white dark:bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-xl" style={{ width: '100%', position: 'relative' }}>
+                  <Image src={selfCheckout} layout="responsive" objectFit="cover" />
+                </div>
+              </div>
+              <div className="w-full lg:w-1/3 p-2">
+                <div className="shadow-2xl rounded-lg overflow-hidden bg-white dark:bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-xl" style={{ width: '100%', position: 'relative' }}>
+                  <Image src={selfCheckout} layout="responsive" objectFit="cover" />
+                </div>
+              </div>
+              <div className=" w-full p-6 mx-auto mt-10 shadow-2xl rounded-xl overflow-hidden bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 dark:from-gray-800 dark:via-gray-900 dark:to-black text-gray-100 dark:text-gray-200 transform transition-all duration-500 hover:scale-105">
+                <h3 className="text-4xl py-2 text-gray-300 font-medium dark:text-gray-400 md:text-4xl">
+                  Self-Checkout Kiosk App – Wonderla
+                </h3>
+                <p className="py-2 text-white">
+                  Developed a self-checkout kiosk app for Wonderla, enabling guests to order food and items independently with digital payments, reducing queue times and improving operational efficiency.                </p>
+                <div className="flex justify-center mt-6">
+                  {/* <a
+                    className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md"
+                    href="#"
+                    onClick={handleRedirectNoNames}
+                  >
+                    Live App
+                  </a> */}
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
+          {/* <div className="lg:flex gap-5">
+            <div className="text-start md:p-10 rounded-xl mt-10 flex-1 flex flex-wrap" style={{ height: 'auto' }}>
+              <div className="w-full lg:w-1/3 p-2">
+                <div className="shadow-2xl rounded-lg overflow-hidden bg-white dark:bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-xl" style={{ width: '100%', position: 'relative' }}>
+                  <Image src={qq_3} layout="responsive" objectFit="cover" />
+                </div>
+              </div>
+
+              <div className="w-full lg:w-1/3 p-2">
+                <div className="shadow-2xl rounded-lg overflow-hidden bg-white dark:bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-xl" style={{ width: '100%', position: 'relative',}}>
+                  <Image src={qq_1} layout="responsive" objectFit="cover" />
+                </div>
+              </div>
+              <div className="w-full lg:w-1/3 p-2">
+                <div className="shadow-2xl rounded-lg overflow-hidden bg-white dark:bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-xl," style={{ width: '88%', position: 'relative', padding: '20px' , paddingTop: '40px', height: '100%'}}>
+                  <Image src={qq_2} layout="responsive" objectFit="cover" />
+                </div>
+              </div>
+              <div className=" w-full p-6 mx-auto mt-10 shadow-2xl rounded-xl overflow-hidden bg-gradient-to-br from-purple-600 via-pink-500 to-red-500 dark:from-gray-800 dark:via-gray-900 dark:to-black text-gray-100 dark:text-gray-200 transform transition-all duration-500 hover:scale-105">
+                <h3 className="text-4xl py-2 text-gray-300 font-medium dark:text-gray-400 md:text-4xl">
+                  QR-based Restaurant Ordering System
+                </h3>
+                <p className="py-2 text-white">
+                 QR-based self-ordering system for dine-in restaurants. Customers scan table QR codes to access the menu, place orders, and pay. The system also allows for table management, order tracking, and payment collection.
+                </p>
+                <div className="flex justify-center mt-6">
+                  <a
+                    className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md"
+                    href="#"
+                    onClick={handleRedirectQQ}
+                  >
+                    Live App
+                  </a>
+                </div>
+              </div>
+
+
+            </div>
+          </div> */}
         </section>
-<TechStack />
+        <TechStack />
         <footer className="text-gray-800 dark:text-gray-200 py-12 mt-20">
           <div className="container mx-auto px-4">
             <div className="flex flex-col md:flex-row justify-between items-center mb-8">
